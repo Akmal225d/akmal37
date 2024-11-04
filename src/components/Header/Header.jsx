66a8d6dda3dc2b5cb9f200a4a1628a5661328a6b
Akmal225d <islombahodirov424@gmail.com>
2024-11-04 15:13:31 +0500

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="head">
+          <h1 className="logo">IT-ACADEMY</h1>
+          <nav>
+            <a href="#">Курсы</a>
+            <a href="#">Бесплатный урок</a>
+            <a href="#">Филиалы</a>
+            <a href="#">Контакты</a>
+          </nav>
+
+          <button>ПОЗВОНИТЬ</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
